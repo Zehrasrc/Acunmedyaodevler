48c2bebda37c386236d72aba94084a7700bcc093
Zehrasrc <sercezehra2004@gmail.com>
2025-04-18 12:20:33 +0300

    Serialization&Deserialization_Odevi

---
+++ Serialization & Deserialization/Serialization & Deserialization/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization___Deserialization
+{
+    public class Urun
+    {
+        public string UrunAdi { get; set; }
+        public double Fiyat { get; set; }
+        public string Kategori { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
