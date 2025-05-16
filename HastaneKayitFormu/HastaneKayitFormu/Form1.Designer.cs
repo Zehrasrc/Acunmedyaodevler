781ad22122ae6d08b0d35f53aa2e9fb7d305162b
Zehrasrc <sercezehra2004@gmail.com>
2025-05-16 17:56:25 +0300

    HastaneKayitFormuOdevi

---
+++ HastaneKayitFormu/HastaneKayitFormu/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HastaneKayitFormu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbBrans = new System.Windows.Forms.ComboBox();
+            this.cmbDoktor = new System.Windows.Forms.ComboBox();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.cmbSaat = new System.Windows.Forms.ComboBox();
+            this.txtHastaAdi = new System.Windows.Forms.TextBox();
+            this.txtHastaSoyadi = new System.Windows.Forms.TextBox();
+            this.btnRandevuOlustur = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbBrans
+            // 
+            this.cmbBrans.FormattingEnabled = true;
+            this.cmbBrans.Location = new System.Drawing.Point(120, 21);
+            this.cmbBrans.Name = "cmbBrans";
+            this.cmbBrans.Size = new System.Drawing.Size(121, 24);
+            this.cmbBrans.TabIndex = 0;
+            // 
+            // cmbDoktor
+            // 
+            this.cmbDoktor.FormattingEnabled = true;
+            this.cmbDoktor.Location = new System.Drawing.Point(120, 79);
+            this.cmbDoktor.Name = "cmbDoktor";
+            this.cmbDoktor.Size = new System.Drawing.Size(121, 24);
+            this.cmbDoktor.TabIndex = 1;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(41, 121);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(200, 22);
+            this.dtpTarih.TabIndex = 2;
+            // 
+            // cmbSaat
+            // 
+            this.cmbSaat.FormattingEnabled = true;
+            this.cmbSaat.Location = new System.Drawing.Point(123, 154);
+            this.cmbSaat.Name = "cmbSaat";
+            this.cmbSaat.Size = new System.Drawing.Size(118, 24);
+            this.cmbSaat.TabIndex = 3;
+            // 
+            // txtHastaAdi
+            // 
+            this.txtHastaAdi.Location = new System.Drawing.Point(125, 195);
+            this.txtHastaAdi.Name = "txtHastaAdi";
+            this.txtHastaAdi.Size = new System.Drawing.Size(116, 22);
+            this.txtHastaAdi.TabIndex = 4;
+            // 
+            // txtHastaSoyadi
+            // 
+            this.txtHastaSoyadi.Location = new System.Drawing.Point(125, 232);
+            this.txtHastaSoyadi.Name = "txtHastaSoyadi";
+            this.txtHastaSoyadi.Size = new System.Drawing.Size(116, 22);
+            this.txtHastaSoyadi.TabIndex = 5;
+            // 
+            // btnRandevuOlustur
+            // 
+            this.btnRandevuOlustur.Location = new System.Drawing.Point(69, 276);
+            this.btnRandevuOlustur.Name = "btnRandevuOlustur";
+            this.btnRandevuOlustur.Size = new System.Drawing.Size(204, 39);
+            this.btnRandevuOlustur.TabIndex = 6;
+            this.btnRandevuOlustur.Text = "button1";
+            this.btnRandevuOlustur.UseVisualStyleBackColor = true;
+            this.btnRandevuOlustur.Click += new System.EventHandler(this.btnRandevuOlustur_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(279, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 303);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRandevuOlustur);
+            this.Controls.Add(this.txtHastaSoyadi);
+            this.Controls.Add(this.txtHastaAdi);
+            this.Controls.Add(this.cmbSaat);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.cmbDoktor);
+            this.Controls.Add(this.cmbBrans);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBrans;
+        private System.Windows.Forms.ComboBox cmbDoktor;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.ComboBox cmbSaat;
+        private System.Windows.Forms.TextBox txtHastaAdi;
+        private System.Windows.Forms.TextBox txtHastaSoyadi;
+        private System.Windows.Forms.Button btnRandevuOlustur;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ HastaneKayitFormu/HastaneKayitFormu/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace HastaneKayitFormu
+{
+    public partial class Form1 : Form
+    {
+        // Veritabanı bağlantı dizesi
+        string connectionString = @"Data Source=LAPTOP-7FOARPM6;Initial Catalog=RandevuSistemi;Integrated Security=SSPI;TrustServerCertificate=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                BranslariListele();
+                SaatleriYukle();
+                RandevulariListele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yükleme Hatası: " + ex.Message);
+            }
+        }
+
+        // Branşları ComboBox'a yükleme
+        void BranslariListele()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Branslar", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmbBrans.DataSource = dt;
+                    cmbBrans.DisplayMember = "BransAdi";
+                    cmbBrans.ValueMember = "Id";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Branş Yükleme Hatası: " + ex.Message);
+            }
+        }
+
+        // Doktorları ComboBox'a yükleme
+        void DoktorlariListele(int bransId)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter($"SELECT * FROM Doktorlar WHERE BransID={bransId}", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmbDoktor.DataSource = dt;
+                    cmbDoktor.DisplayMember = "DoktorAdi";
+                    cmbDoktor.ValueMember = "Id";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Doktor Yükleme Hatası: " + ex.Message);
+            }
+        }
+
+        // Branş seçildiğinde doktorları listeleme
+        private void cmbBrans_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbBrans.SelectedValue is int bransId)
+            {
+                DoktorlariListele(bransId);
+            }
+        }
+
+        // Randevu saatlerini yükleme
+        void SaatleriYukle()
+        {
+            string[] saatler = { "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00" };
+            cmbSaat.Items.AddRange(saatler);
+            cmbSaat.SelectedIndex = 0;
+        }
+
+        // Randevuları DataGridView üzerinde listeleme
+        void RandevulariListele()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(
+                        "SELECT r.HastaAdi, r.HastaSoyadi, b.BransAdi, d.DoktorAdi, r.Tarih " +
+                        "FROM Randevular r " +
+                        "JOIN Branslar b ON r.BransID = b.Id " +
+                        "JOIN Doktorlar d ON r.DoktorID = d.Id", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Randevu Listeleme Hatası: " + ex.Message);
+            }
+        }
+
+        // Randevu oluşturma
+        private void btnRandevuOlustur_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtHastaAdi.Text) || string.IsNullOrWhiteSpace(txtHastaSoyadi.Text))
+            {
+                MessageBox.Show("Hasta adı ve soyadı boş bırakılamaz!");
+                return;
+            }
+
+            int bransId = Convert.ToInt32(cmbBrans.SelectedValue);
+            int doktorId = Convert.ToInt32(cmbDoktor.SelectedValue);
+            DateTime tarih = dtpTarih.Value.Date.Add(TimeSpan.Parse(cmbSaat.Text));
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    // Randevu çakışma kontrolü
+                    SqlCommand checkCmd = new SqlCommand(
+                        "SELECT COUNT(*) FROM Randevular WHERE DoktorID=@doktorId AND Tarih=@tarih", con);
+                    checkCmd.Parameters.AddWithValue("@doktorId", doktorId);
+                    checkCmd.Parameters.AddWithValue("@tarih", tarih);
+
+                    int count = (int)checkCmd.ExecuteScalar();
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Bu doktor için bu tarihte randevu zaten var!");
+                        return;
+                    }
+
+                    // Randevu kaydı
+                    SqlCommand cmd = new SqlCommand(
+                        "INSERT INTO Randevular (HastaAdi, HastaSoyadi, BransID, DoktorID, Tarih) " +
+                        "VALUES (@adi, @soyadi, @bransId, @doktorId, @tarih)", con);
+                    cmd.Parameters.AddWithValue("@adi", txtHastaAdi.Text);
+                    cmd.Parameters.AddWithValue("@soyadi", txtHastaSoyadi.Text);
+                    cmd.Parameters.AddWithValue("@bransId", bransId);
+                    cmd.Parameters.AddWithValue("@doktorId", doktorId);
+                    cmd.Parameters.AddWithValue("@tarih", tarih);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Randevu başarıyla oluşturuldu!");
+                    RandevulariListele();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Randevu Oluşturma Hatası: " + ex.Message);
+            }
+        }
+    }
+}
